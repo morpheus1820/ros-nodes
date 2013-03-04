3d5f8ba4c5514bebf8464e3b0f022dc9f79b2571
Stefano Rosa <stefano.rosa@polito.it>
2013-03-04 09:25:58 +0100

    first commit

---
+++ vfh/CMakeFiles/vfh_node.dir/DependInfo.cmake
@@ -0,0 +1,14 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/p3dx/ros_workspace/labrob-polito-ros-pkg/vfh/src/vfh_algorithm.cc" "/home/p3dx/ros_workspace/labrob-polito-ros-pkg/vfh/CMakeFiles/vfh_node.dir/src/vfh_algorithm.o"
+  "/home/p3dx/ros_workspace/labrob-polito-ros-pkg/vfh/src/vfh_node.cpp" "/home/p3dx/ros_workspace/labrob-polito-ros-pkg/vfh/CMakeFiles/vfh_node.dir/src/vfh_node.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
---
+++ vfh/CMakeFiles/vfh_node.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/vfh_node.dir/src/vfh_node.o"
+  "CMakeFiles/vfh_node.dir/src/vfh_algorithm.o"
+  "bin/vfh_node.pdb"
+  "bin/vfh_node"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/vfh_node.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
